6b45eb59c664aa1aeb4b1cf48a405eb129250f3a
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-11-24 04:28:13 +0200

    gallery desktop

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,20 @@
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
 
 import 'swiper/css';
 
-const gallerySwiper = new Swiper('.gallery-swiper', {
+const galleryPhoneSwiper = new Swiper('.gallery-swiper', {
   loop: true,
   slidesPerView: 1.5,
+});
+
+const galleryDesktopSwiper = new Swiper('.gallery-desktop-swiper', {
+  modules: [Navigation],
+  loop: true,
+  slidesPerView: 5,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
