d538c5185d251e3a55cb83a3f0cc9d5f5e97ca73
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-11-25 21:54:17 +0200

    try to do gallery swiper

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,5 @@
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css/navigation';
-
-import 'swiper/css';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 const galleryPhoneSwiper = new Swiper('.gallery-swiper', {
   loop: true,
@@ -10,9 +7,18 @@ const galleryPhoneSwiper = new Swiper('.gallery-swiper', {
 });
 
 const galleryDesktopSwiper = new Swiper('.gallery-desktop-swiper', {
-  modules: [Navigation],
+  effect: 'coverflow',
   slidesPerView: 5,
   loop: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 50,
+    depth: 150,
+    modifier: 1,
+    slideShadows: false,
+  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
