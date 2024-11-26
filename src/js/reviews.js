dfa711a4b2334256780337ad796795ca7b36e797
Olena457 <olenademianenko42@gmail.com>
2024-11-26 21:34:56 +0200

    added adaptive photos

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,17 +1,35 @@
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+
+// const reviewsPhoneSwiper = new Swiper('.reviews-phone-swiper-select', {
+//   loop: true,
+//   slidesPerView: 1,
+//   spaceBetween: 50,
+// });
+
+// const reviewsDesktopSwiper = new Swiper('.reviews-desktop-swiper-select', {
+//   slidesPerView: 2.5,
+
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+// VARIANT-2_____________________________________________________________________________
+
 import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
 
-const reviewsPhoneSwiper = new Swiper('.reviews-phone-swiper-select', {
-  loop: true,
+const swiper3 = new Swiper('.swiper-container-3', {
   slidesPerView: 1,
-  spaceBetween: 50,
-});
-
-const reviewsDesktopSwiper = new Swiper('.reviews-desktop-swiper-select', {
-  slidesPerView: 2.5,
-
+  spaceBetween: 30,
+  loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-container-3 .swiper-button-next',
+    prevEl: '.swiper-container-3 .swiper-button-prev',
+  },
+  breakpoints: {
+    1200: { slidesPerView: 3, navigation: true },
+    0: { navigation: false, slidesPerView: 1 },
   },
 });
